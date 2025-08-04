7b4c7717695ec22f60f27ff278cd55cd48bd0034
callcolor <call.color@gmail.com>
2025-08-04 14:15:41 -0500

    feat: pre-commit script and github action

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,7 +18,7 @@ const advocates = pgTable("advocates", {
   specialties: jsonb("payload").default([]).notNull(),
   yearsOfExperience: integer("years_of_experience").notNull(),
   phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
 
 export { advocates };
