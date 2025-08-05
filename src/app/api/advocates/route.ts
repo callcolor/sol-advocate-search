cf22dc51663ea6ab1cf5b8308858cd39068d0dc9
callcolor <call.color@gmail.com>
2025-08-05 08:34:40 -0500

    feat: filtering on advocates/get endpoint

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,47 @@
+import { NextRequest } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
+import { desc, and, gte, like, eq, sql } from "drizzle-orm";
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const firstName = searchParams.get('firstName');
+  const lastName = searchParams.get('lastName');
+  const city = searchParams.get('city');
+  const degree = searchParams.get('degree');
+  const specialties = searchParams.getAll('specialties');
+  const yearsOfExperience = searchParams.get('yearsOfExperience');
+  const limit = Number(searchParams.get('limit')) || 10;
+  const offset = Number(searchParams.get('offset')) || 0;
+
+  const filters = [];
+  if (firstName) {
+    filters.push(like(advocates.firstName, firstName))
+  }
+  if (lastName) {
+    filters.push(like(advocates.lastName, lastName))
+  }
+  if (city) {
+    filters.push(like(advocates.city, city))
+  }
+  if (degree) {
+    filters.push(eq(advocates.degree, degree))
+  }
+  if (yearsOfExperience) {
+    filters.push(gte(advocates.yearsOfExperience, Number(yearsOfExperience)))
+  }
+  if (specialties) {
+    const specialtiesArray = JSON.stringify(specialties);
+    filters.push(sql`payload @> ${specialtiesArray}`)
+  }
+
+  const data = await db.
+    select()
+    .from(advocates)
+    .where(and(...filters))
+    .orderBy(desc(advocates.yearsOfExperience))
+    .limit(limit)
+    .offset(offset);
 
   return Response.json({ data });
 }
